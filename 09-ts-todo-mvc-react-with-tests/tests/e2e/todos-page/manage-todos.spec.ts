f3da0d49b3048ad56c96d17f4b8d8e211dcb327e
Miras <71659835+Serikuly-Miras@users.noreply.github.com>
2021-03-25 00:29:38 +0300

    Added cypress todo-card deletion test

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
@@ -18,6 +18,8 @@ function authorize() {
     .wait('@initialTodos');
 }
 
+const itemText = Math.random().toString() + '_' + Date.now();
+
 describe('Manage Todos', () => {
   context('Creation', () => {
     it('Create todo', () => {
@@ -38,8 +40,6 @@ describe('Manage Todos', () => {
         ).as('createTodo');
       });
 
-      const itemText = Math.random().toString() + '_' + Date.now();
-
       authorize();
       cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
       cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
@@ -53,6 +53,37 @@ describe('Manage Todos', () => {
       cy.get('[data-test-id=todo-item__checked-checkbox]').should('not.be.checked');
       cy.get('[data-test-id=todo-item__text-input]').should('have.value', itemText);
       cy.get('[data-test-id=todo-item__remove-action]').should('be.visible');
+    });
+  });
+
+  context('Delete todo cards', () => {
+    it('Delete one', () => {
+      cy.fixture('todo-item.response.json').then((todoItemResponse)  => {
+        cy.intercept(
+          'POST',
+          todosEndpoint,
+          req => {
+            const { body } = req;
+            req.reply({
+              statusCode: 200,
+              body: JSON.stringify({
+                ...todoItemResponse,
+                text: body.text
+              })
+            });
+          }
+        ).as('createTodo');
+      });
+
+      authorize();
+      cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
+      cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
+      cy.get('[data-test-id=create-new-todo-form]').submit();
+
+      cy.wait('@createTodo');
+      cy.get('[data-test-id=todo-item__remove-action]').click({force: true});
+      cy.get('[data-test-id=todo-item]').should('not.exist');
+      cy.get('[data-test-id=todos-list]').should('be.empty');
     });
   });
 });
